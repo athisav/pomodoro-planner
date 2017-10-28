e24a207fb3396796a035c81647732982572a4081
rumboogy <rumboogy@users.noreply.github.com>
2017-10-28 10:13:30 -0700

    Revert "BROKEN VERSION"
    This reverts commit a5ebe8ca8262b496a0fb14fd2ff79dc48d0d98c6.

--- public/js/inputManager.js
+++ public/js/inputManager.js
@@ -1,4 +1,3 @@
-//all of this is for the dynamic input modification
 //getting the place that will be modified
 var block_collection = document.getElementById("assignmentBlockCollection")
 
@@ -13,21 +12,6 @@ function addInputBlock() {
   e.innerHTML = input_block
 
   block_collection.appendChild(e)
-}
-
-//all of this is for storing data
-//used for maintaing a optimized list of assignments and dates
-function arrayUnique(array) {
-    var a = array.concat()
-    var b = []
-    for(var i=0; i<a.length; ++i) {
-        for(var j=i+1; j<a.length; ++j) {
-            if(a[i] === a[j])
-                a.splice(j--, 1)
-                b.push(j)
-        }
-    }
-    return [a, b]
 }
 
 function saveInputInfo() {
@@ -45,16 +29,5 @@ function saveInputInfo() {
   work_data = {}
   work_data.assignments = assignments
   work_data.dates = dates
-  existing_data = localStorage.getItem("data")
-  var merger_data = arrayUnique(existing_data.assignments.concat(work_data.assignments))
-  var final_data = []
-  final_data.assignments = merger_data[0]
-  final_data.dates = work_data.dates
-  for (var i=0; i<merger_data[1].length; i++) {
-    final_data.dates.splice(merger_data[1][i]--, 1)
-  }
-  localStorage.setItem("data", JSON.stringify(final_data))
-
-function deleteData() {
-  localStorage.setItem("data", " ")
+  console.log(work_data)
 }
