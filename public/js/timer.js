f3576f8851de5babe38907c59b18bbde4e4434ad
star1954 <andidiwang@yahoo.com>
2017-10-29 09:28:25 -0700

    Formatted Timer Code, should reduce chance of random bugs

--- public/js/timer.js
+++ public/js/timer.js
@@ -1,42 +1,40 @@
-paired_data = []
+paired_data = [];
 
 function refresh_data () {
-  data = localStorage.getItem("data")
-  data = JSON.parse(data)
+  data = localStorage.getItem("data");
+  data = JSON.parse(data);
 
   for (var i=0; i<data.assignments.length; i++) {
-    paired_data.push({assignment : data.assignments[i], days_till_due : data.days_till_due[i]})
+    paired_data.push({assignment : data.assignments[i], days_till_due : data.days_till_due[i]});
   }
 
-  sorted_status = false
+  sorted_status = false;
 
-  while (sorted_status=false) {
-    sorted_status = true
+  while (sorted_status==false) {
+    sorted_status = true;
     for (var i=0; i<paired_data.length; i++) {
-      if (i+1!=paired_data.length) {
+      if (i+1!==paired_data.length) {;
         if (paired_data[i].days_till_due>paired_data[i+1].days_till_due) {
-          var d1 = paired_data[i]
-          var d2 = paired_data[i+1]
-          paired_data[i] = d2
-          paired_data[i+1] = d1
-          sorted_status = false
-        }
+          var d1 = paired_data[i];
+          var d2 = paired_data[i+1];
+          paired_data[i] = d2;
+          paired_data[i+1] = d1;
+          sorted_status = false;
       }
     }
   }
-}
+};
 
-refresh_data()
+refresh_data();
 
 function main_loop () {
-  refresh_data()
-  assignment_display = document.getElementById("timer_assignment")
-  assignment_display.innerHTML = "'" + "Get Ready!" + "'"
-  button = document.getElementById("timer-button-2")
-  button.removeEventListener("click", loop)
-  button.remove()
-  timer = document.getElementById("timer")
-
+  refresh_data();
+  assignment_display = document.getElementById("timer_assignment");
+  assignment_display.innerHTML = "'" + "Get Ready!" + "'";
+  button = document.getElementById("timer-button-2");
+  button.removeEventListener("click", loop);
+  button.remove();
+  timer = document.getElementById("timer");
   var countdown = 10;
   var state = 0;
 
@@ -45,34 +43,34 @@ function main_loop () {
       var x = i;
       var y = 0;
   while(x>=60){
-      x-= 60
+      x-= 60;
       y++;
   }
-  return([y,x])
+  return([y,x]);
   }
 
   function init(){
       var time = 10;
       if(state == 0){
-          console.log("aaa")
+          console.log("aaa");
           //assigment
-          assignment_display.innerHTML = "'" + paired_data[0].assignment + "'"
+          assignment_display.innerHTML = "'" + paired_data[0].assignment + "'";
           //
-          state = 1
+          state = 1;
           time = 10;
       }else{
-        assignment_display.innerHTML = "'" + "Break Time!" + "'"
-          paired_data.shift()
-          state = 0
+        assignment_display.innerHTML = "'" + "Break Time!" + "'";
+          paired_data.shift();
+          state = 0;
       }
       countdown = time;
-      refresh_data()
+      refresh_data();
   }
 
   function update(){
       countdown--;
       if(countdown<1){
-          timer.innerHTML = "Count down is done!"
+          timer.innerHTML = "Count down is done!";
           return 1;
       }else{
           timer.innerHTML = second_minute(countdown)[0] + " : " + second_minute(countdown)[1];
@@ -81,10 +79,10 @@ function main_loop () {
   }
   function loop(){
       if(update()){
-          init()
+          init();
       }
   }
-  setInterval(loop,1000)
+  setInterval(loop,1000);
 }
 
-document.getElementById("timer-button-2").addEventListener("click", main_loop)
+document.getElementById("timer-button-2").addEventListener("click", main_loop);
